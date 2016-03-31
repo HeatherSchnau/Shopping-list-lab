1d06608fd348685cc1838c15b976d53c9b1dc123
Heather Schnau <nouveauhousewife@gmail.com>
2016-03-30 21:25:15 -0400

    Initial commit for lab 6

---
+++ bootcampexamples.js
@@ -0,0 +1,22 @@
+var banana = {
+  name: "banana",
+  price: 4
+}
+
+var pear = {
+  name: "pear",
+  price: 1
+}
+
+var grapes = {
+  name: "grapes",
+  price: 2
+}
+
+var grocery = [banana, pear, grapes];
+var sum = 0;
+grocery.forEach(function(element){
+	console.log(element.name + ': ' + element.price);
+  sum = sum + element.price;
+}
+console.log('total: ' + sum);
