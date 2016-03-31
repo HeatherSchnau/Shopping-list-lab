a077cf8f90621d6ec2eb626e2917ed508ceb5f5d
Heather Schnau <nouveauhousewife@gmail.com>
2016-03-30 21:42:53 -0400

    Fixed in text editor

--- bootcampexamples.js
+++ bootcampexamples.js
@@ -1,19 +1,17 @@
-var banana = {
+var groceries = [
+ {
   name: "banana",
   price: 4
-}
-
-var pear = {
+},
+{
   name: "pear",
   price: 1
-}
-
-var grapes = {
+},
+{
   name: "grapes",
   price: 2
-}
+}];
 
-var grocery = [banana, pear, grapes];
 var sum = 0;
 grocery.forEach(function(element){
 	console.log(element.name + ': ' + element.price);
