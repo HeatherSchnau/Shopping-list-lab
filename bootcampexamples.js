41eeed2730b5dedaedb0ecb8203f2ae4279a2748
Heather Schnau <nouveauhousewife@gmail.com>
2016-04-09 21:23:52 -0400

    summation of grocery prices

--- bootcampexamples.js
+++ bootcampexamples.js
@@ -13,8 +13,19 @@ var groceries = [
 }];
 
 var sum = 0;
+
+while(groceries.length > 0) {
+  var item = groceries.pop()
+  console.log(item.name + ': ' + item.price);
+  sum = sum + item.price;
+}
+console.log('total: ' + sum);
+
+/*
 grocery.forEach(function(element){
 	console.log(element.name + ': ' + element.price);
   sum = sum + element.price;
 });
+
 console.log('total: ' + sum);
+*/
